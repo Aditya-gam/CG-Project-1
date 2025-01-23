251afea4c5138c9bd4945257ba4446e3dc55bcc2
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2025-01-23 13:53:05 -0800

    Implement reflective shading logic, fixing compile errors and adding recursion-based reflection combination in Reflective_Shader.

--- rt/reflective_shader.cpp
+++ rt/reflective_shader.cpp
@@ -1,27 +1,22 @@
 #include "reflective_shader.h"
-#include "parse.h"
 #include "ray.h"
 #include "render_world.h"
 
-Reflective_Shader::Reflective_Shader(const Parse* parse,std::istream& in)
-{
-    in>>name;
-    shader=parse->Get_Shader(in);
-    in>>reflectivity;
-    reflectivity=std::max(0.0,std::min(1.0,reflectivity));
-}
-
-// vec3 Reflective_Shader::
-// Shade_Surface(const Render_World& render_world,const Ray& ray,const Hit& hit,
-//     const vec3& intersection_point,const vec3& normal,int recursion_depth) const
-// {
-//     TODO; // determine the color
-//     return {};
-// }
-
 vec3 Reflective_Shader::
-Shade_Surface(const Ray& ray, const vec3& intersection_point, const vec3& normal, int recursion_depth) const
+Shade_Surface(const Ray& ray,const vec3& intersection_point,
+    const vec3& normal,int recursion_depth) const
 {
-    TODO; // determine the color
-    return {};
+    vec3 color = shader->Shade_Surface(ray, intersection_point, normal, recursion_depth);
+
+    vec3 v_ray = ray.direction;
+    vec3 r_dir = 2.0 * dot(-v_ray, normal) * normal + v_ray;
+    Ray r_ray(intersection_point, r_dir);
+
+    if(recursion_depth < world.recursion_depth_limit) {
+        color = (1 - reflectivity) * color + reflectivity * world.Cast_Ray(r_ray, ++recursion_depth);
+    }
+    else
+        color = (1-reflectivity) * color;
+
+    return color;
 }
