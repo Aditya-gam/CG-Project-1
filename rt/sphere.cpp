c304ad7ef52358d4f80db6920fe2f138c448eb40
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2025-01-23 13:57:43 -0800

    Implement sphere intersection logic (discriminant-based solution) and normal calculation; fix compile-time issues in Sphere class.

--- rt/sphere.cpp
+++ rt/sphere.cpp
@@ -1,26 +1,38 @@
 #include "sphere.h"
 #include "ray.h"
 
-Sphere::Sphere(const Parse* parse, std::istream& in)
-{
-    in>>name>>center>>radius;
-}
-
 // Determine if the ray intersects with the sphere
 Hit Sphere::Intersection(const Ray& ray, int part) const
 {
-    TODO;
-    return {};
+    double discriminant = pow(dot(ray.direction, ray.endpoint - center), 2) - (dot(ray.direction, ray.direction) * (dot(ray.endpoint - center, ray.endpoint - center) - pow(radius, 2)));
+    double t_1 = 0;
+    double t_2 = 0;
+
+    if (discriminant > 0) {
+        t_1 = -dot(ray.direction, ray.endpoint - center) + sqrt(discriminant) / dot(ray.direction, ray.direction);
+        t_2 = -dot(ray.direction, ray.endpoint - center) - sqrt(discriminant) / dot(ray.direction, ray.direction);
+
+        if (t_1 < t_2 && t_1 >= small_t) {
+            return {this, t_1, part};
+        }
+        else if (t_1 >= t_2 && t_2 >= small_t){
+            return {this, t_2, part};
+        }
+    }
+
+    return {nullptr, t_1, part};
 }
 
-vec3 Sphere::Normal(const Ray& ray, const Hit& hit) const
+vec3 Sphere::Normal(const vec3& point, int part) const
 {
     vec3 normal;
-    TODO; // compute the normal direction
+    normal = (point - center) / radius;
     return normal;
 }
 
-std::pair<Box,bool> Sphere::Bounding_Box(int part) const
+Box Sphere::Bounding_Box(int part) const
 {
-    return {{center-radius,center+radius},false};
+    Box box;
+    TODO; // calculate bounding box
+    return box;
 }
