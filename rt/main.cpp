fb84af869079c8079100fe390c4721af3a66db02
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2025-01-23 14:03:23 -0800

    Fix main rendering pipeline: properly parse input, handle debug pixel rendering, and compare against solution. Resolve compile and linkage issues in main driver.

--- rt/main.cpp
+++ rt/main.cpp
@@ -1,15 +1,12 @@
-#include "dump_png.h"
-#include "object.h"
-#include "parse.h"
 #include "render_world.h"
-#include <cstdio>
-#include <fstream>
+#include "object.h"
 #include <iostream>
+#include <cstdio>
 #include <unistd.h>
 
 /*
 
-  Usage: ./ray_tracer -i <test-file> [ -s <solution-file> ] [ -o <output-file> ] [ -x <debug-x-coord> -y <debug-y-coord> ]
+  Usage: ./ray_tracer -i <test-file> [ -s <solution-file> ] [ -o <stats-file> ] [ -x <debug-x-coord> -y <debug-y-coord> ]
 
   Examples:
 
@@ -39,114 +36,98 @@
   quantities are being computed correctly and can help you narrow down your
   search.
 
-  The -f flag is used by the grading script.  It causes the results of your ray
+  The -o flag is used by the grading script.  It causes the results of your ray
   tracer to be printed to a file rather than to the standard output.  This
   prevents the grading script from getting confused by debugging output.
 
-  The -o flag can be used to specify the output file.  The default is
-  output.png.
-
-  The -h flag disables the acceleration structure.  This is useful for testing
-  and debugging the acceleration structure.
-
-  The -z flag changes the resolution of the acceleration structure.  This is
-  useful for testing correctness, runtime performance, and scaling.
  */
 
 // Indicates that we are debugging one pixel; can be accessed everywhere.
-bool Debug_Scope::enable=false;
-int Debug_Scope::level=0;
-bool enable_acceleration=true;
-int acceleration_grid_size=40;
+bool debug_pixel=false;
+
+// This can be used to quickly disable the hierarchy for testing purposes.
+// Though this is not required, it is highly recommended that you implement
+// this, as it will make debugging your hierarchy much easier.
+bool disable_hierarchy=false;
 
 void Usage(const char* exec)
 {
-    std::cerr<<"Usage: "<<exec<<" -i <test-file> [ -s <solution-file> ] [ -f <stats-file> ] [ -o <output-file> ] [ -x <debug-x-coord> -y <debug-y-coord> ] [ -h ]  [ -z <resolution> ] "<<std::endl;
+    std::cerr<<"Usage: "<<exec<<" -i <test-file> [ -s <solution-file> ] [ -o <stats-file> ] [ -x <debug-x-coord> -y <debug-y-coord> ]"<<std::endl;
     exit(1);
 }
 
-void Setup_Parsing(Parse& parse);
+void Parse(Render_World& world,int& width,int& height,const char* test_file);
+void Dump_png(Pixel* data,int width,int height,const char* filename);
+void Read_png(Pixel*& data,int& width,int& height,const char* filename);
 
 int main(int argc, char** argv)
 {
     const char* solution_file = 0;
     const char* input_file = 0;
-    const char* output_file = "output.png";
     const char* statistics_file = 0;
     int test_x=-1, test_y=-1;
 
     // Parse commandline options
     while(1)
     {
-        int opt = getopt(argc, argv, "s:i:o:f:x:y:hz:");
+        int opt = getopt(argc, argv, "s:i:m:o:x:y:h");
         if(opt==-1) break;
         switch(opt)
         {
             case 's': solution_file = optarg; break;
             case 'i': input_file = optarg; break;
-            case 'f': statistics_file = optarg; break;
-            case 'o': output_file = optarg; break;
+            case 'o': statistics_file = optarg; break;
             case 'x': test_x = atoi(optarg); break;
             case 'y': test_y = atoi(optarg); break;
-            case 'h': enable_acceleration=false; break;
-            case 'z': acceleration_grid_size = atoi(optarg); break;
+            case 'h': disable_hierarchy=true; break;
         }
     }
     if(!input_file) Usage(argv[0]);
 
-    Render_World render_world;
+    int width=0;
+    int height=0;
+    Render_World world;
     
     // Parse test scene file
-    Parse parse;
-    Setup_Parsing(parse);
-    
-    std::ifstream fin(input_file);
-    if(!fin)
-    {
-        std::cerr<<"Error: Failed to open file "<<input_file<<std::endl;
-        exit(1);
-    }
-    assert(fin);
-    parse.Parse_Input(render_world,fin);
-    
+    Parse(world,width,height,input_file);
+
     // Render the image
-    render_world.Render();
+    world.Render();
 
     // For debugging.  Render only the pixel specified on the commandline.
     // Useful for printing out information about a single pixel.
     if(test_x>=0 && test_y>=0)
     {
         // Set a global variable to indicate that we are debugging one pixel.
-        // This way you can do: debug.Print("foo = ",foo);
-        Debug_Scope::enable = true;
-        std::cout<<"debug pixel: -x "<<test_x<<" -y "<<test_y<<std::endl;
+        // This way you can do: if(debug_pixel) cout<<lots<<of<<stuff<<here<<endl;
+        debug_pixel = true;
 
         // Render just the pixel we are debugging
-        render_world.Render_Pixel(ivec2(test_x,test_y));
+        world.Render_Pixel(ivec2(test_x,test_y));
 
         // Mark the pixel we are testing green in the output image.
-        render_world.camera.Set_Pixel(ivec2(test_x,test_y),0x00ff00ff);
+        world.camera.Set_Pixel(ivec2(test_x,test_y),0x00ff00ff);
     }
 
     // Save the rendered image to disk
-    Dump_png(render_world.camera.colors,render_world.camera.number_pixels[0],render_world.camera.number_pixels[1],output_file);
-    
+    Dump_png(world.camera.colors,width,height,"output.png");
+
     // If a solution is specified, compare against it.
     if(solution_file)
     {
-        int width = 0, height = 0;
+        int width2 = 0, height2 = 0;
         Pixel* data_sol = 0;
 
         // Read solution from disk
-        Read_png(data_sol,width,height,solution_file);
-        assert(render_world.camera.number_pixels[0]==width);
-        assert(render_world.camera.number_pixels[1]==height);
+        Read_png(data_sol,width2,height2,solution_file);
+        assert(width==width2);
+        assert(height==height2);
 
         // For each pixel, check to see if it matches solution
         double error = 0, total = 0;
         for(int i=0; i<height*width; i++)
         {
-            vec3 a=From_Pixel(render_world.camera.colors[i]);
+            vec3 a=From_Pixel(world.camera.colors[i]);
             vec3 b=From_Pixel(data_sol[i]);
             for(int c=0; c<3; c++)
             {
@@ -172,4 +153,3 @@ int main(int argc, char** argv)
 
     return 0;
 }
-
